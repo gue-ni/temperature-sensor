83383903730a68718540c757e04bfc6d8626deef
Jakob <45669207+gue-ni@users.noreply.github.com>
2024-08-16 08:15:23 +0200

    Update upload_data.sh

--- upload_data.sh
+++ upload_data.sh
@@ -2,11 +2,29 @@
 set -e
 set -x
 
-today=$(date "+%Y-%m-%d")
-log_file=/home/pi/temperature-sensor/log/temperature.$today.log
+workspace=/home/pi/temperature-sensor
+
+date=$(date "+%Y-%m-%d")
+log_file=$workspace/log/temperature.$date.log
 tmp_file=/tmp/temperature.txt
 
-tail -n 1 $log_file > $tmp_file
+data=$(tail -n 1 $log_file)
 
-scp $tmp_file $1
+echo $data > $tmp_file
 
+timestamp=$(echo $data | awk '{ print $1 }')
+temperature=$(echo $data | awk '{ print $2 }')
+humidity=$(echo $data | awk '{ print $3 }')
+
+mkdir -p $workspace/www
+cp $workspace/template.html $workspace/www/index.html
+
+python $workspace/analyze.py $log_file $workspace/www/plot.png
+
+sed -i 's/__TIMESTAMP__/$date/g'  $workspace/www/index.html
+sed -i 's/__TEMPERATURE__/$temperature/g'  $workspace/www/index.html
+sed -i 's/__HUMIDITY__/$humidity/g'  $workspace/www/index.html
+sed -i 's/__IMAGE__/plot.png/g'  $workspace/www/index.html
+
+#scp $tmp_file $1
+scp $workspace/www $1
