b8bb42b356a53f8031ad90a6e0cfaeeaa110341a
gue-ni <jakob.g.maier@gmail.com>
2024-08-16 11:19:17 +0200

    update

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ pip install -r requirements.txt
 
 # install service
 cp temperature-service.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl start temperature-service.service
 sudo systemctl enable temperature-service.service
 ```
--- upload_data.sh
+++ upload_data.sh
@@ -7,6 +7,7 @@ workspace=/home/pi/temperature-sensor
 date=$(date "+%Y-%m-%d")
 log_file=$workspace/log/temperature.$date.log
 tmp_file=/tmp/temperature.txt
+now=$(date)
 
 data=$(tail -n 1 $log_file)
 
@@ -18,12 +19,15 @@ timestamp=${array[0]}
 temperature=${array[1]}
 humidity=${array[2]}
 
-image_name=plot.png
+uuid=$(cat /proc/sys/kernel/random/uuid)
+image_name=plot-$uuid.png
 
 rm -rf $workspace/www
 mkdir -p $workspace/www
 cp $workspace/template.html $workspace/www/index.html
 
+sed -i "s/__NOW__/${now}/g"  $workspace/www/index.html
+sed -i "s/__TIMESTAMP__/${timestamp}/g"  $workspace/www/index.html
 sed -i "s/__TIMESTAMP__/${timestamp}/g"  $workspace/www/index.html
 sed -i "s/__TEMPERATURE__/${temperature}/g"  $workspace/www/index.html
 sed -i "s/__HUMIDITY__/${humidity}/g"  $workspace/www/index.html
@@ -32,6 +36,6 @@ sed -i "s/__IMAGE__/${image_name}/g" $workspace/www/index.html
 /home/pi/temperature-sensor/venv/bin/python $workspace/analyze.py $log_file $workspace/www/$image_name
 
 server=root@jakobmaier.at
-webroot=/var/www/files/temperature-sensor
+webroot=/var/www/project/temperature-sensor
 
 rsync --archive --verbose --delete $workspace/www/ $server:$webroot
