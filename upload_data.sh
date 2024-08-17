03489195cc340331661b38cc582c8835d744e26f
jakob <jakob.g.maier@gmail.com>
2024-08-17 09:14:51 +0200

    update

--- upload_data.sh
+++ upload_data.sh
@@ -5,7 +5,8 @@ set -x
 workspace=/home/pi/temperature-sensor
 
 date=$(date "+%Y-%m-%d")
-log_file=$workspace/log/temperature.$date.log
+log_dir=$workspace/log
+log_file=$log_dir/temperature.$date.log
 tmp_file=/tmp/temperature.txt
 now=$(date)
 
@@ -20,8 +21,9 @@ temperature=${array[1]}
 humidity=${array[2]}
 hostname=$(hostname)
 
-uuid=$(cat /proc/sys/kernel/random/uuid)
-image_name=plot-$uuid.png
+#uuid=$(cat /proc/sys/kernel/random/uuid)
+image_1=img-$(cat /proc/sys/kernel/random/uuid).png
+image_2=img-$(cat /proc/sys/kernel/random/uuid).png
 
 rm -rf $workspace/www
 mkdir -p $workspace/www
@@ -32,12 +34,14 @@ sed -i "s/__TIMESTAMP__/${timestamp}/g"  $workspace/www/index.html
 sed -i "s/__TIMESTAMP__/${timestamp}/g"  $workspace/www/index.html
 sed -i "s/__TEMPERATURE__/${temperature}/g"  $workspace/www/index.html
 sed -i "s/__HUMIDITY__/${humidity}/g"  $workspace/www/index.html
-sed -i "s/__IMAGE__/${image_name}/g" $workspace/www/index.html
+sed -i "s/__IMAGE_1__/${image_1}/g" $workspace/www/index.html
+sed -i "s/__IMAGE_2__/${image_2}/g" $workspace/www/index.html
 sed -i "s/__HOSTNAME__/${hostname}/g" $workspace/www/index.html
 
-/home/pi/temperature-sensor/venv/bin/python $workspace/analyze.py $log_file $workspace/www/$image_name
+/home/pi/temperature-sensor/venv/bin/python $workspace/analyze.py $log_file $workspace/www/$image_1
+/home/pi/temperature-sensor/venv/bin/python $workspace/analyze.py $log_dir/* $workspace/www/$image_2
 
 server=root@jakobmaier.at
-webroot=/var/www/project/temperature-sensor-2
+webroot=/var/www/project/temperature-sensor
 
 rsync --archive --verbose --delete $workspace/www/ $server:$webroot
